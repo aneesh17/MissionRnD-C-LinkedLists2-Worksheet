118357a5d83f8be6fa9ad34c617dedf529c1acf1
aneesh17 <aneesh17@missionrnd.com>
2016-01-29 02:03:09 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,12 +12,66 @@ NOTES:
 */
 
 #include <stdio.h>
+#include<malloc.h>
 
 struct node {
 	int num;
 	struct node *next;
 };
 
+struct node* newNode(int ele)
+{
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	newnode->num = ele;
+	newnode->next = NULL;
+	return(newnode);
+}
+
+void addNewnum(struct node** newnode, int ele)
+{
+	struct node* temp = *newnode;
+	if (*newnode == NULL)
+	{
+		(*newnode) = newNode(ele);
+		(*newnode)->next = NULL;
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = (struct node*)malloc(sizeof(struct node));
+		temp->next->num = ele;
+		temp->next->next = NULL;
+	}
+}
+
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
+	struct node* result = NULL;
+	while (head1 != NULL && head2 != NULL)
+	{
+		if (head1->num<head2->num)
+		{
+			addNewnum(&result, head1->num);
+			head1 = head1->next;
+		}
+		else
+		{
+			addNewnum(&result, head2->num);
+			head2 = head2->next;
+		}
+	}
+	while (head1 != NULL)
+	{
+		addNewnum(&result, head1->num);
+		head1 = head1->next;
+	}
+	while (head2 != NULL)
+	{
+		addNewnum(&result, head2->num);
+		head2 = head2->next;
+	}
+	return(result);
 	return NULL;
 }
