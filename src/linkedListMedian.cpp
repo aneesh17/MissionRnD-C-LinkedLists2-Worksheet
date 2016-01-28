31bec9422b205bd7b5660adb745bee4fb7109582
aneesh17 <aneesh17@missionrnd.com>
2016-01-29 00:37:43 +0530

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -1,11 +1,11 @@
 /*
-OVERVIEW: Given a single sorted linked list find the median element of the single linked list.
-Median is the middle element.
+OVERVIEW: Given a single sorted linked list find the median num of the single linked list.
+Median is the middle num.
 E.g.: 1->2->3->4->5, output is 3.
 
 INPUTS: A sorted linked list.
 
-OUTPUT: Return median of the linked list elements.
+OUTPUT: Return median of the linked list nums.
 
 ERROR CASES: Return -1 for error cases.
 
@@ -20,5 +20,26 @@ struct node {
 };
 
 int linkedListMedian(struct node *head) {
+
+	if (head != NULL)
+	{
+		struct node* temp = head;
+		int numOfNodes = 0, counter;
+		int result;
+		while (temp->next != NULL)
+		{
+			numOfNodes++;
+			temp = temp->next;
+		}
+		counter = (numOfNodes) / 2;
+
+		while (counter>0)
+		{
+			head = head->next;
+			counter--;
+		}
+		result = (numOfNodes % 2 == 1) ? ((head->num + head->next->num) / 2) : (head->num);
+		return(result);
+	}
 	return -1;
 }
